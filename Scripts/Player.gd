7b49cbcc2c715e01fe1955d16f9cd7307f0613ff
Andrei Jardan <jardan.andrei@gmail.com>
2018-06-09 17:47:13 +0300

    Added shitty shooting

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,27 @@
 extends KinematicBody2D
 
 const SPEED = 20
+var pot = preload("res://Mini Scenes/pot.tscn")
+var root
 
 func _ready():
 	set_physics_process(true)
+	root = get_tree().get_root().get_node("Game")
 
-func _physics_process(delta):
+func _movement():
 	var velocity = Vector2()
 	var input = Vector2()
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	velocity = input.normalized() * SPEED
 	position += velocity
+
+func _shooting():
+	if Input.is_action_just_pressed("char_shoot"):
+		root.add_child(pot.instance())
+		pass
+	pass
+
+func _physics_process(delta):
+	_movement()
+	_shooting()

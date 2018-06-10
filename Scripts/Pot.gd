0bc91a0ae273a59ef31db3bc4a6912ab7b21829f
Mathe <andreidracusorul@gmail.com>
2018-06-11 01:01:05 +0300

    broken pot

--- Scripts/Pot.gd
+++ Scripts/Pot.gd
@@ -1,13 +1,31 @@
 extends RigidBody2D
 
+var broken_pot_sprite = preload("res://Textures/BrokenPotSprite.png")
+
+
+# VARIABLES
+const BROKE_DELAY = 0.15
+
 
 func _ready():
 	set_physics_process(true)
 	
+	# TIMER INITIALIZATION
+	$Timer.set_one_shot(true)
+	$Timer.set_wait_time(BROKE_DELAY)
+	$Timer.connect("timeout", self, "on_timeout_complete")
+	
+func on_timeout_complete():
+	queue_free()
+
+
 func _physics_process(delta):
 	var bodies = get_colliding_bodies()
 	
 	for body in bodies:
-		if body.is_in_group("Enemies"):
+		if body.is_in_group("Enemies") or body.is_in_group("Pots"):
+			$Sprite.set_texture(broken_pot_sprite)
+			linear_velocity = Vector2(0, 0)
+			angular_velocity = 0
+			$Timer.start()
 			body.free()
-			free()

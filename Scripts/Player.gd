b522e9de935839ffba122335893a00503b410c87
Andrei Jardan <jardan.andrei@gmail.com>
2018-06-11 00:26:41 +0300

    Prettified shooting

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,30 +6,18 @@ var root
 # VARIABLES
 const SPEED = 20
 const SHOOT_DELAY = 0.2
+#TODO: move this to pot.gd
 const POT_SPEED = 500
 
-var can_shoot = null
-var timer = null
-
+var can_shoot = true
 
 func _ready():
 	set_physics_process(true)
 	root = get_tree().get_root().get_node("Game")
-	
-	can_shoot = false
-	
-	# TIMER INITIALIZATION
-	timer = Timer.new()
-	timer.set_one_shot(true)
-	timer.set_wait_time(SHOOT_DELAY)
-	timer.connect("timeout", self, "on_timeout_complete")
-	add_child(timer)
-	timer.start()
 
 # If timer stopped
-func on_timeout_complete():
+func _on_Timer_timeout():
 	can_shoot = true
-
 
 func _movement():
 	var velocity = Vector2()
@@ -39,19 +27,16 @@ func _movement():
 	velocity = input.normalized() * SPEED
 	move_and_collide(velocity)
 
-func _shooting():
-	if Input.is_action_pressed("char_shoot"):
-		var pot_instance = pot.instance()
-		pot_instance.position = position
-		var angle = get_angle_to(get_viewport().get_mouse_position())
-		pot_instance.linear_velocity = Vector2(cos(angle), sin(angle)) * POT_SPEED
-		root.add_child(pot_instance)
-	
-	timer.start()
+func _shoot():
+	var pot_instance = pot.instance()
+	pot_instance.position = position
+	var angle = get_angle_to(get_viewport().get_mouse_position())
+	pot_instance.linear_velocity = Vector2(cos(angle), sin(angle)) * POT_SPEED
+	root.add_child(pot_instance)
+	$Timer.start()
 
 func _physics_process(delta):
 	_movement()
-	if can_shoot:
-		_shooting()
+	if Input.is_action_pressed("char_shoot") && can_shoot:
+		_shoot()
 		can_shoot = false
-	
